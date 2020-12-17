c382113faa168470e6dfa2a4dd5273dc09fc2ecc
EladRon <elad.ron.g@gmail.com>
2020-12-17 22:38:24 +0200

    updating to useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,13 +1,42 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Text, View, StyleSheet, Button, FlatList } from 'react-native';
 
+const COUNTER_INCREASE = 'COUNTER_INCREASE';
+const COUNTER_DECREASE = 'COUNTER_DECREASE';
+
+const reducer = (count, action) => {
+    switch (action.type) {
+        case COUNTER_INCREASE:
+            return count + action.payload;
+        case COUNTER_DECREASE:
+            return count - action.payload;
+    }
+};
+
 const CounterScreen = () => {
-    const [count, setCount] = useState(0);
+    const [count, dispatch] = useReducer(reducer, 0);
+    const COUNTER_CHANGE_INTERVAL = 1;
 
     return (
         <View>
-            <Button title='Increase' onPress={() => setCount(count + 1)} />
-            <Button title='Decrease' onPress={() => setCount(count - 1)} />
+            <Button
+                title='Increase'
+                onPress={() =>
+                    dispatch({
+                        type: COUNTER_INCREASE,
+                        payload: COUNTER_CHANGE_INTERVAL
+                    })
+                }
+            />
+            <Button
+                title='Decrease'
+                onPress={() =>
+                    dispatch({
+                        type: COUNTER_DECREASE,
+                        payload: COUNTER_CHANGE_INTERVAL
+                    })
+                }
+            />
             <Text>Current Count: {count}</Text>
         </View>
     );
