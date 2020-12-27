5ea27a8c7b7025c5a0f78e6bce3c7968707d347b
EladRon <elad.ron.g@gmail.com>
2020-12-27 15:31:12 +0200

    Adding validation to Text

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -3,6 +3,8 @@ import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
     const [name, setName] = useState('');
+    const [pass, setPass] = useState('');
+
     return (
         <View>
             <Text>Enter Name:</Text>
@@ -14,6 +16,16 @@ const TextScreen = () => {
                 onChangeText={(newValue) => setName(newValue)}
             />
             <Text>My name is {name}</Text>
+            <Text>Enter Passward:</Text>
+            <TextInput
+                style={styles.input}
+                autoCapitalize='none'
+                autoCorrect={false}
+                onChangeText={(newValue) => setPass(newValue)}
+            />
+            {pass.length > 4 ? null : (
+                <Text>Passward must be longet than 5 characters</Text>
+            )}
         </View>
     );
 };
