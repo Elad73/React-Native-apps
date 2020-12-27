aea6c1a3fc7180776252c704d94b7ae65c5f7b55
EladRon <elad.ron.g@gmail.com>
2020-12-27 18:33:52 +0200

    wrapping box,flex and position

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,10 +3,19 @@ import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 const BoxScreen = () => {
     return (
-        <View style={styles.viewStyle}>
-            <Text style={styles.text1Style}>Box #1</Text>
-            <Text style={styles.text2Style}>Box #2</Text>
-            <Text style={styles.text3Style}>Box #3</Text>
+        <View>
+            <View style={styles.viewStyle}>
+                <Text style={styles.text1Style}>Box #1</Text>
+                <Text style={styles.text2Style}>Box #2</Text>
+                <Text style={styles.text3Style}>Box #3</Text>
+            </View>
+            <View style={styles.viewBox}>
+                <View style={styles.viewBoxRow}>
+                    <View style={styles.box1}></View>
+                    <View style={styles.box2}></View>
+                </View>
+                <View style={styles.box3}></View>
+            </View>
         </View>
     );
 };
@@ -24,11 +33,43 @@ const styles = StyleSheet.create({
     text2Style: {
         borderWidth: 3,
         borderColor: 'red',
-        alignSelf: 'center'
+        ...StyleSheet.absoluteFillObject
     },
     text3Style: {
         borderWidth: 3,
         borderColor: 'red'
+    },
+    viewBox: {
+        borderWidth: 3,
+        borderColor: 'blue'
+    },
+    viewBoxRow: {
+        borderWidth: 3,
+        borderColor: 'blue',
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+    box1: {
+        borderWidth: 3,
+        borderColor: 'red',
+        backgroundColor: 'orange',
+        width: 100,
+        height: 100
+    },
+    box2: {
+        borderWidth: 3,
+        borderColor: 'red',
+        backgroundColor: 'pink',
+        width: 100,
+        height: 100
+    },
+    box3: {
+        borderWidth: 3,
+        borderColor: 'red',
+        backgroundColor: 'green',
+        alignSelf: 'center',
+        width: 100,
+        height: 100
     }
 });
 
